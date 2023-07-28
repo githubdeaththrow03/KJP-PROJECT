7c7d6544d9d823cffbc6dd18b0ef49c590eb9f3f
Paolo Rossin <112543287+githubdeaththrow03@users.noreply.github.com>
2023-07-28 13:18:19 +0800

    fixed responsiveness

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -105,7 +105,7 @@ return (
 <div className='home-main-container'>
     <NavHome />
     <div className="highlight-Homepage ren" style={{ backgroundColor: '#38117A' }}>
-        <div className='container-fluid d-flex align-items-center justify-content-between'>
+        <div className='card-nav'>
             <div className="search-card">
                 <input type="text" placeholder="Search Username" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onKeyPress={enterSearch}/>
             </div>
